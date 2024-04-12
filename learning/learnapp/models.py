5799ce14b5a68f87389f3f3492f9a31cddfc5af6
asparkswghs <90475848+asparkswghs@users.noreply.github.com>
2024-04-12 11:16:37 -0500

    add teacher model

--- learning/learnapp/models.py
+++ learning/learnapp/models.py
@@ -17,3 +17,29 @@ class Student(models.Model):
 
     def __str__(self):
         return f'{self.last_name}, {self.first_name}'
+
+class Teacher(models.Model):
+
+    TITLES = (
+        ('Mr', 'Mr'),
+        ('Mrs', 'Mrs'),
+        ('Ms', 'Ms'),
+        ('Miss', 'Miss'),
+        ('Mx', 'Mx'),
+    )
+
+    DEPARTMENTS = (
+        ('English', 'English'),
+        ('Math', 'Math'),
+        ('Business', 'Business'),
+        ('Family and Consumer Sciences', 'Family and Consumer Sciences'),
+        ('Social Studies', 'Social Studies'),
+        ('Science', 'Science'),
+        ('Foreign Language', 'Foreign Language'),
+        ('Physical Education', 'Physical Education')
+    )
+
+    first_name = models.CharField(max_length=200, null=True)
+    last_name = models.CharField(max_length=200, null=True)
+    title = models.CharField(max_length=10, null=True, choices=TITLES)
+    department = models.CharField(max_length=30, null=True, choices=DEPARTMENTS)
